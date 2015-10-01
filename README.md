5133126bb08de518795e9ef179c39f75366fe81e
The Dark Knight <dknight@VDG.local>
2015-10-01 16:22:27 -0400

    Updated viper.py

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Viper Brood
 An Application Template for Data Science in Python
+
+Goal: To create a universal template for data science workflow as it
+
+pertains to python. The purpose is to predifine as much boilerplate code
+
+as possible subscribing to a functional programming paridigm allowing aspiring
+
+and current data scientists to focus on data science while learning the particulars
+
+of the python programming language. Use and modify this template as you see fit and
+
+please fork this project on github. All improvements / suggestions are wellcome.
+
+Many Thanks, Victor Green Data Engineer / Data Scientist
--- viper.py
+++ viper.py
@@ -7,13 +7,6 @@ Data Science with python.
 Author: Victor Green, vgreen@gds.org
 Date: 09-30-2015
 
-Goal: To create a universal template for data science workflow as it 
-pertains to python. The purpose is to predifine as much boilerplate code
-as possible subscribing to a functional programming paridigm allowing aspiring
-and current data scientists to focus on data science while learning the particulars
-of the python programming language. Use and modify this template as you see fit and please
-fork this project on github. All improvements / suggestions are wellcome.
-Many Thanks, Victor Green Data Engineer / Data Scientist
 """
 
 
@@ -24,6 +17,103 @@ import sys
 import getopt
 import pygal
 
+
+
+
+#### OVERRIDE THIS FUNCTION HERE THIS IS WHERE YOUR
+def run_code( input_file,output_file,output_image,show_results ):
+
+	# 1: GET AND CLEAN DATA
+	raw_data = get_parse_clean_data_as_csv_1( input_file )
+
+	# 2: DEFINE ALL DATA FIELDS IN FILE
+	data_fields = []
+	data_fields.append("airline")
+	data_fields.append("avail_seat_km_per_week")
+	data_fields.append("incidents_85_99")
+	data_fields.append("fatal_accidents_85_99")
+	data_fields.append("fatalities_85_99")
+	data_fields.append("incidents_00_14")
+	data_fields.append("fatal_accidents_00_14")
+	data_fields.append("fatalities_00_14")
+	clean_data = convert_list_of_lists_to_list_of_dicts( data_fields,raw_data[1:] )
+
+
+	# 3: ANALYZE DATA
+	results = analyze_data_1( clean_data )
+
+
+	# 4: DISPLAY RESULTS
+	if show_results:
+		print results
+
+
+	# 5: SAVE RESULTS IF NAME OF OUTPUT FILE GIVE
+	if output_file != '':
+		save_data_1( results, output_file )
+
+
+	# 6: VISUALIZE DATA
+	if output_image != '':
+		visualize_data_1( results,output_image )
+
+#### CUSTOM DATA SCIENCE LOGIC AND WORKFLOW GOES
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####BEGIN DATA ANALYSIS FUNCTIONS HERE
+def analyze_data_1( data ):
+	"""PERFORMS 1ST LEG OF ANALYSIS
+	AND RETURNS RESULTS AS A LIST OF TUPLES
+	"""
+
+	results = []
+
+	for airline in data:
+
+		avg_incidents = int( airline["incidents_85_99"] ) + int( airline["incidents_00_14"] ) / 30
+		results.append( ( airline["airline"],avg_incidents) )
+
+	return results
+####END DATA ANALYSIS FUNCTIONS HERE
+
+
+
+####BEGIN DATA VISUALIZATION FUNCTIONS HERE
+def visualize_data_1( data,chart_name ):
+	"""TAKES A LIST OF TUPLES
+	CREATES A BAR CHART
+	"""
+
+	bar_chart = pygal.Bar()
+	bar_chart.title = "Average Airline Incidents"
+
+	for row in data:
+		bar_chart.add(row[0],[ row[1] ])
+
+	bar_chart.render_to_file(chart_name)
+####END DATA VISUALIZATION FUNCTIONS
 
 
 
@@ -38,6 +128,20 @@ def get_parse_clean_data_as_csv_1( file_name ):
 		data = []
 		for row in f:
 			data.append( row.split(',') )
+
+		return data
+
+
+def get_parse_clean_data_as_tsv_1( file_name ):
+	"""PARSES TSV FILE INTO A SINGLE LIST THAT CONTAINS
+	EACH ROW IN A SEPARATE LIST OF STRINGS CONTAINING EACH
+	FIELD FOR THAT ROW. LIST OF LISTS
+	"""
+
+	with open( file_name, 'rU' ) as f:
+		data = []
+		for row in f:
+			data.append( row.split('\t') )
 
 		return data
 
@@ -93,29 +197,6 @@ def convert_list_of_lists_to_list_of_dicts( k,values ):
 
 
 
-####BEGIN DATA ANALYSIS FUNCTIONS HERE
-def analyze_data_1( data ):
-	"""PERFORMS 1ST LEG OF ANALYSIS
-	AND RETURNS RESULTS AS A LIST OF TUPLES
-	"""
-
-	results = []
-
-	for airline in data:
-
-		avg_incidents = int( airline["incidents_85_99"] ) + int( airline["incidents_00_14"] ) / 30
-		results.append( ( airline["airline"],avg_incidents) )
-
-	return results
-####END DATA ANALYSIS FUNCTIONS HERE
-
-
-
-####BEGIN MACHINE LEARNING / DATA SCIENCE ALGORITHMS DEFINITIONS HERE
-####END MACHINE LEARNING / DATA SCIENCE ALGORITHMS DEFINITIONS HERE
-
-
-
 ####BEGIN SAVE DATA TO FILE FUNCTIONS HERE
 def save_data_1( data, output_file ):
 	"""SAVES THE RESULTS OF YOUR
@@ -129,7 +210,6 @@ def save_data_1( data, output_file ):
 	#INTERPOLATION
 	#"%s" % ( data, )
 
-
 	fo = open( output_file, "wb" )
 	fo.write( str( data ) );
 	fo.close()
@@ -137,20 +217,8 @@ def save_data_1( data, output_file ):
 
 
 
-####BEGIN DATA VISUALIZATION FUNCTIONS HERE
-def visualize_data_1( data,chart_name ):
-	"""TAKES A LIST OF TUPLES
-	CREATES A BAR CHART
-	"""
-
-	bar_chart = pygal.Bar()
-	bar_chart.title = "Average Airline Incidents"
-
-	for row in data:
-		bar_chart.add(row[0],[ row[1] ])
-
-	bar_chart.render_to_file(chart_name)
-####END DATA VISUALIZATION FUNCTIONS
+####BEGIN MACHINE LEARNING / DATA SCIENCE ALGORITHMS DEFINITIONS HERE
+####END MACHINE LEARNING / DATA SCIENCE ALGORITHMS DEFINITIONS HERE
 
 
 
@@ -163,17 +231,18 @@ def main( argv ):
 	input_file = ''
 	output_file = ''
 	output_image = ''
+	show_results = False
 
 
 	#PARSE COMMAND LINE OPTIONS
 	#RAISE ERROR IF OPTION IS NOT VALID
 	#OR IF REQUIRED ARGUMENT DOES FOLLOW OPTION
 	try:
-		opts, args = getopt.getopt( argv, "hi:o:v:",["help","inputfile=","outputfile=","outputimage="] )
+		opts, args = getopt.getopt( argv, "hsi:o:v:",["help","showresults","inputfile=","outputfile=","outputimage="] )
 	except getopt.GetoptError:
 		print ''
-		print 'viper.py -i <input_file> -o <output_file> -v <output_image>'
-		print 'viper.py --inputfile <input_file> --outputfile <output_file> --outputimage <output_image>'
+		print 'viper.py -i <input_file> -o <output_file> -v <output_image> -s'
+		print 'viper.py --inputfile <input_file> --outputfile <output_file> --outputimage <output_image> --showresults'
 		print ''
 		sys.exit(2)
 
@@ -195,49 +264,20 @@ def main( argv ):
 			output_file = arg
 		elif opt in ("-v","--outputimage"):
 			output_image = arg
+		elif opt in ("-s","--showresults"):
+			show_results = True
 
 
 	#CHECK TO SEE IF ANY (REQUIRED) COMMAND LINE OPTION(S) HAVE BEEN SET
-	if input_file == '' or output_image == '':
+	if input_file == '':
 		print ''
-		print 'viper.py -i <input_file> -v <output_image>'
-		print 'viper.py --inputfile <input_file> --outputimage <output_image>'
+		print 'viper.py -i <input_file>'
+		print 'viper.py --inputfile <input_file>'
 		print ''
 		sys.exit(3)
 
-
-
-	#1: GET AND CLEAN DATA
-	raw_data = get_parse_clean_data_as_csv_1( input_file )
-
-	data_fields = []
-	data_fields.append("airline")
-	data_fields.append("avail_seat_km_per_week")
-	data_fields.append("incidents_85_99")
-	data_fields.append("fatal_accidents_85_99")
-	data_fields.append("fatalities_85_99")
-	data_fields.append("incidents_00_14")
-	data_fields.append("fatal_accidents_00_14")
-	data_fields.append("fatalities_00_14")
-
-	clean_data = convert_list_of_lists_to_list_of_dicts( data_fields,raw_data[1:] )
-
-
-	#2: ANALYZE DATA
-	results = analyze_data_1( clean_data )
-
-
-	#3: DISPLAY RESULTS
-	#print results
-
-
-	#4: SAVE RESULTS IF NAME OF OUTPUT FILE GIVE
-	if output_file != '':
-		save_data_1( results, output_file )
-
-
-	#5: VISUALIZE DATA
-	visualize_data_1( results,output_image )
+	#RUN YOU ANALYSIS
+	run_code( input_file,output_file,output_image,show_results )
 ####END MAIN DEFINITION
 
 
